b65fe0b235507c3593221c418b889d3d12fac0a2
sghwan <anan123456@naver.com>
2022-10-05 11:28:08 +0900

    입력 받은 값에 대한 구구단 출력

--- src/Gugudan.java
+++ src/Gugudan.java
@@ -1,9 +1,15 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
 public class Gugudan {
-	public static void main(String[] args) {
-		for(int i = 2; i <= 3; i++) {
-			for(int j = 1; j <= 9; j++) {
-				System.out.println(i * j);
-			}
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int dan = Integer.parseInt(br.readLine());
+		
+		for(int i = 1; i <= 9; i++) {
+			int result = dan * i;
+			System.out.println(result);
 		}
 	}
 }
