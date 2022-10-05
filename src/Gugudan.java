e58b2ea60cedbed1d2516c9b434c2deeb234f625
sghwan <anan123456@naver.com>
2022-10-05 13:43:49 +0900

    메소드를 활용한 구구단 출력

--- src/Gugudan.java
+++ src/Gugudan.java
@@ -1,16 +1,26 @@
 import java.io.IOException;
 
 public class Gugudan {
-	public static void main(String[] args) throws IOException{
+	public static int[] calculate(int times) {
 		int[] result = new int[9];
 		
+		for(int i = 0; i < 9; i++) {
+			result[i] = times * (i + 1);
+		}
+		
+		return result;
+	}
+	
+	public static void print(int[] result) {
+		for(int i = 0; i < 9; i++) {
+			System.out.println(result[i]);
+		}
+	}
+	
+	public static void main(String[] args) throws IOException{
+
 		for(int i = 2; i < 10; i++) {
-			System.out.println(i + "단");
-			for(int j = 1; j <= 9; j++) {
-				result[j - 1] = i * j;
-				System.out.println(i + " * " + j + " = " + result[j - 1]);
-			}
-			System.out.println("=================");
+			print(calculate(i));
 		}
 	}
 }
