1629394b6b25143f46eed7e0b40dbab778dc8298
sghwan <anan123456@naver.com>
2022-10-05 11:09:00 +0900

    initial commit

---
+++ src/Gugudan.java
@@ -0,0 +1,9 @@
+public class Gugudan {
+	public static void main(String[] args) {
+		for(int i = 2; i <= 3; i++) {
+			for(int j = 1; j <= 9; j++) {
+				System.out.println(i * j);
+			}
+		}
+	}
+}
