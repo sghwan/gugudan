f46154cf9b6acc13ec8f27aeb81f5c204ae4f407
sghwan <anan123456@naver.com>
2022-10-05 12:00:34 +0900

    입력한 값이 조건에 맞으면 그 값에 대한 구구단 출력

--- src/Gugudan.java
+++ src/Gugudan.java
@@ -7,6 +7,11 @@ public class Gugudan {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int num = Integer.parseInt(br.readLine());
 		
+		if(num < 2 || num > 9) {
+			System.out.println("2이상, 9이하의 값만 입력할 수 있습니다.");
+			return;
+		}
+		
 		for(int i = 1; i <= 9; i++) {
 			int result = num * i;
 			System.out.println(result);
